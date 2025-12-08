8592e5be0a4011445ce506fd941ef95134af4a6e
barbieribbruna <barbieribbruna@gmail.com>
2025-12-08 19:19:26 -0300

    registrando entidade em produto.module

--- src/produto/produto.module.ts
+++ src/produto/produto.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ProdutoService } from './produto.service';
 import { ProdutoController } from './produto.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Produto } from './entities/produto.entity';
 
 @Module({
-  controllers: [ProdutoController],
-  providers: [ProdutoService],
+
+    imports: [TypeOrmModule.forFeature([Produto])],
+    controllers: [ProdutoController],
+    providers: [ProdutoService],
+    exports: [TypeOrmModule]
 })
 export class ProdutoModule {}
