8c0108e9e1eeda4a9af19bdf3714d83de436b2d5
barbieribbruna <barbieribbruna@gmail.com>
2025-12-08 11:24:26 -0300

    install primeiros pacotes TypeORM, MySQL e Validation + config Classe Main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+async function bootstrap() {    // função principal que inicia a aplicação Nest
+
+    const app = await NestFactory.create(AppModule);    // cria a aplicação baseada no AppModule, que é o módulo raiz do projeto
+
+    process.env.TZ = '-03:00'   // define o fuso horário da aplicação, usado no Brasil
+
+    app.useGlobalPipes(new ValidationPipe())    // ativa validação automática dos DTOs em todas as rotas
+
+    app.enableCors();   // habilita CORS (mecanismo de segurança que controle quem pode acessar a API) para permitir requisições de outros domínios
+
+    await app.listen(process.env.PORT ?? 3000); // inicia o servidor na porta definida ou padrão 3000
 }
-bootstrap();
+bootstrap();    // executa a função inicial
