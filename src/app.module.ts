2b2b495152637074b7a288fb10560799c81f6c86
barbieribbruna <barbieribbruna@gmail.com>
2025-12-08 12:21:33 -0300

    Configuração do TypeORM com .env e MySQL no app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+
+        ConfigModule.forRoot({  // habilita uso de variáveis de ambiente (.env)
+            isGlobal: true,     // permite acessar process.env em qualquer arquivo do projeto
+        }),
+
+        TypeOrmModule.forRoot({
+            type: 'mysql',      // define que o banco de dados usado será o MySQL
+            host: process.env.DB_HOST,      // endereço do servidor do banco (localhost)
+            port: Number(process.env.DB_PORT),      // porta onde o MySQL está rodando
+            username: process.env.DB_USERNAME,      // usuário do MySQL
+            password: process.env.DB_PASSWORD,      // senha do usuário do MySQL, retirada do .env para não aparecer no GitHub
+            database: process.env.DB_NAME,      // nome do banco de dados que a aplicação vai usar
+            autoLoadEntities: true,     // carrega automaticamente todas as entidades do projeto, sem precisar registrar cada entidade manualmente
+            synchronize: true,      // faz o TypeORM criar e atualizar as tabelas conforme as entidades
+        }),
+    ],
+    controllers: [],
+    providers: [],
 })
-export class AppModule {}
+export class AppModule { }
