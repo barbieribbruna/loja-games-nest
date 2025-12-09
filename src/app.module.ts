56fc984eef9b11703bd01b70520df966336ab03d
barbieribbruna <barbieribbruna@gmail.com>
2025-12-09 10:45:53 -0300

    Configura TypeORM para usar variáveis de ambiente do sistema

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,13 +13,16 @@ import { CategoriaModule } from './categoria/categoria.module';
 
         TypeOrmModule.forRoot({
             type: 'mysql',      // define que o banco de dados usado será o MySQL
-            host: process.env.DB_HOST,      // endereço do servidor do banco (localhost)
-            port: Number(process.env.DB_PORT),      // porta onde o MySQL está rodando
-            username: process.env.DB_USERNAME,      // usuário do MySQL
-            password: process.env.DB_PASSWORD,      // senha do usuário do MySQL, retirada do .env para não aparecer no GitHub
-            database: process.env.DB_NAME,      // nome do banco de dados que a aplicação vai usar
+            
+            // essas informações abaixo, para preservar a password, foram inseridas como variáveis de ambiente do Windows e facilitam caso haja alteração futura de senha
+            host: process.env.DB_HOST,                          // endereço do servidor do banco (localhost)
+            port: Number(process.env.DB_PORT),                  // porta onde o MySQL está rodando
+            username: process.env.DB_USER,                      // usuário do MySQL
+            password: process.env.DB_PASSWORD,                  // senha do MySQL, ocultada com .env para não ficar exposta
+            database: 'db_loja_games_nest',                     // nome do banco de dados deste projeto
+            
             autoLoadEntities: true,     // carrega automaticamente todas as entidades do projeto, sem precisar registrar cada entidade manualmente
-            synchronize: true,      // faz o TypeORM criar e atualizar as tabelas conforme as entidades
+            synchronize: true,          // faz o TypeORM criar e atualizar as tabelas conforme as entidades
         }),
 
         ProdutoModule,
